3f36831384e467076bd6f078b48c11a7356ff6cb
Punit Soni <103755013+Punit-99@users.noreply.github.com>
2026-03-24 23:21:43 +0530

    Punit test display data (#14)
    * avatar and card tested
    
    * path error fix
    
    ---------
    
    Co-authored-by: Punit_soni99 <punitsoni028@gmail.com>

--- src/pages/ps.tsx
+++ src/pages/ps.tsx
@@ -2,12 +2,14 @@ import EmptyStateTest from "../component-test/core/EmptyStateTest"
 import ErrorStateTest from "../component-test/core/ErrorStateTest"
 import LayoutShellTest from "../component-test/core/LayoutShellTest"
 import LoaderTest from "../component-test/core/LoaderTest"
+import AvatarTest from "../component-test/data-display/AvatarTest"
 import BadgeTest from "../component-test/data-display/BadgeTest"
+import CardTest from "../component-test/data-display/CardTest"
 import ClipboardTest from "../component-test/data-display/ClipboardTest"
 import DataListTest from "../component-test/data-display/DataListTest"
 import ImageTest from "../component-test/data-display/ImageTest"
 import StatTest from "../component-test/data-display/StatTest"
-// import TableComponentTest from "../component-test/data-display/TableTest"
+// import TableComponentTest from "../component-test/data-display/TableTest""
 import TagExploitTest from "../component-test/data-display/TagTest"
 import AccordionTest from "../component-test/disclosure/AccordionTest"
 import CarouselTest from "../component-test/disclosure/CarouselTest"
@@ -18,25 +20,30 @@ const PS = () => {
     <>
       {/* Core Components */}
       <LoaderTest />
+      <hr />
       <ErrorStateTest />
-      <EmptyStateTest />
-      <LayoutShellTest />
+      <hr />
+      <EmptyStateTest />  <hr />
+      <LayoutShellTest />  <hr />
 
       {/* Display Components */}
-   
-      <BadgeTest/>
-      <ClipboardTest />
-      <DataListTest/>
-      <ImageTest/>
-      <StatTest/>
-      {/* <TableComponentTest/> */}
-      <TagExploitTest/>
+
+      <BadgeTest />  <hr />
+      <ClipboardTest />  <hr />
+      <DataListTest />  <hr />
+      <ImageTest />  <hr />
+      <AvatarTest /> <hr />
+      <StatTest />  <hr />
+      <CardTest /> <hr />
+      {/* <TableComponentTest /> <hr /> */}
+      <TagExploitTest />  <hr />
 
 
       {/* Disclosure Components */}
-      <AccordionTest />
-      <CarouselTest />
-      <CollapsibleTest /> 
+      <AccordionTest />  <hr />
+      <CarouselTest />  <hr />
+      <CollapsibleTest />   <hr />
+
     </>
   )
 }
